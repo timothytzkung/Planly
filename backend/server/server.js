4aba274493ca48651b65a86c968dd9f4df43632e
Tim Kung <timothytzkung@gmail.com>
2026-02-27 14:54:00 -0800

    Feature/Added course outline example
    Added course outline fetch request example

--- backend/server/server.js
+++ backend/server/server.js
@@ -51,7 +51,7 @@ app.post("/api/parse", upload.single("pdf"), async (req, res) => {
   res.json({ transcript: fin });
 });
 
-// SFU API route (req includes => {year, term, department })
+// SFU API courses in dept. route (req includes => {year, term, department })
 app.post("/api/courses", async(req, res) => {
   if (!req.body) return res.status(400).json({ error: "No body attached" });
 
@@ -68,6 +68,39 @@ app.post("/api/courses", async(req, res) => {
   res.json({ courses: data });
 })
 
+// SFU API course sections route (req includes => {year, term, department, courseNumber})
+app.post("/api/course-sections", async(req, res) => {
+  if (!req.body) return res.status(400).json({ error: "No body attached" });
+
+  // Destruct
+  const {year, term, department, courseNumber} = req.body;
+
+  // Fetch
+  const url = `http://www.sfu.ca/bin/wcm/course-outlines?${year}/${term}/${department}/${courseNumber}`
+  const response = await fetch(url, {
+    method: "GET"
+  })
+  // No parsing needed because SFU api parsed it already
+  const data = await response.json();
+  res.json({ courses: data });
+})
+
+// SFU API course outline route (req includes => {year, term, department, courseNumber, courseSection})
+app.post("/api/course-outline", async(req, res) => {
+  if (!req.body) return res.status(400).json({ error: "No body attached" });
+
+  // Destruct
+  const {year, term, department, courseNumber, courseSection} = req.body;
+
+  // Fetch
+  const url = `http://www.sfu.ca/bin/wcm/course-outlines?${year}/${term}/${department}/${courseNumber}/${courseSection}`
+  const response = await fetch(url, {
+    method: "GET"
+  })
+  // No parsing needed because SFU api parsed it already
+  const data = await response.json();
+  res.json({ courses: data });
+})
 
 // Listen
 app.listen(PORT, () => {
--- frontend/src/test/SFUCoursesPage.jsx
+++ frontend/src/test/SFUCoursesPage.jsx
@@ -3,15 +3,14 @@ import { useState, useEffect } from "react";
 
 export const SFUCoursesPage = () => {
   const [err, setErr] = useState("");
-  const [loading, setLoading] = useState(false);
   const [courses, setCourses] = useState(null);
+  const [courseInfo, setCourseInfo] = useState(null);
+  const [courseOutline, setCourseOutline] = useState(null)
 
   const BACK_PORT = 5050;
 
-  const getCourses = async() => {
-    // Load to prevent additional uploads
-    setLoading(true);
-
+  // Get all courses from specific dept in that year
+  const getCourses = async(_year, _term, _department) => {
     // Fetch
     const response = await fetch(`http://localhost:${BACK_PORT}/api/courses`, {
       method: "POST",
@@ -20,27 +19,85 @@ export const SFUCoursesPage = () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ 
-        year: "2026",
-        term: "summer",
-        department: "iat"
+        year: _year,
+        term: _term,
+        department: _department
       })
     }).catch((error) => setErr(error));
 
     // Incoming response already parsed by API
     const data = await response.json();
-    setCourses(data)
+    setCourses(data);
+  }
 
-    // End loading
-    setLoading(false);
+  // get specific course info (returns all course sections available in that term)
+  const getCourseInfo = async(_year, _term, _department, _courseNumber) => {
+    // Fetch
+    const response = await fetch(`http://localhost:${BACK_PORT}/api/course-sections`, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ 
+        year: _year,
+        term: _term,
+        department: _department,
+        courseNumber: _courseNumber
+      })
+    }).catch((error) => setErr(error));
+
+    // Incoming
+    const data = await response.json();
+    setCourseInfo(data);
+  }
+
+  const getCourseOutline = async(_year, _term, _department, _courseNumber, _courseSection) => {
+    // Fetch
+    const response = await fetch(`http://localhost:${BACK_PORT}/api/course-outline`, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ 
+        year: _year,
+        term: _term,
+        department: _department,
+        courseNumber: _courseNumber,
+        courseSection: _courseSection
+      })
+    }).catch((error) => setErr(error));
+
+    // Incoming
+    const data = await response.json();
+    setCourseOutline(data);
   }
 
   // Load on mount
   useEffect(() => {
-    getCourses();
+    getCourses("2026", "summer", "IAT");
+    getCourseInfo("2026", "summer", "IAT", "339");
+    getCourseOutline("2026", "summer", "IAT", "339", "b100");
   }, [])
 
   return (
     <div style={{ maxWidth: 720, margin: "40px auto", padding: 16 }}>
+
+      <h2>Course Outline Info</h2>
+      { courseOutline && (
+        <pre style={{ marginTop: 16, padding: 12, overflowX: "auto" }} >
+          {JSON.stringify(courseOutline, null, 2)}
+        </pre>
+      )}
+
+      <h2>Course Section Info</h2>
+      { courseInfo && (
+        <pre style={{ marginTop: 16, padding: 12, overflowX: "auto" }} >
+          {JSON.stringify(courseInfo, null, 2)}
+        </pre>
+      )}
+
       <h2>Course List</h2>
       {courses && (
         <pre style={{ marginTop: 16, padding: 12, overflowX: "auto" }}>
